5ed217b651d5a2b9fd24f710fcc4eac56f962fb0
pandadreamgs <pandadreamgs@gmail.com>
2026-03-04 19:20:39 +0200

    Add 'red-cross' to the entityList

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let currentLang = 'ua';
 const availableLangs = ['ua', 'en']; 
-const entityList = ['nasa', 'elon-musk', 'bezos', 'netflix', 'south-sudan', 'us-defence-department', 'tokio', 'european-union', 'healthcare-canada'];
+const entityList = ['nasa', 'elon-musk', 'bezos', 'netflix', 'south-sudan', 'us-defence-department', 'tokio', 'european-union', 'healthcare-canada'
+                   'red-cross' ];
 let langDataCache = {}; 
 let currentYear = "2026";
 let currentTimeUnit = "sec";
