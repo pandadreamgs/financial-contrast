4aba5e3f7978099196d13330d27013b75891023b
pandadreamgs <pandadreamgs@gmail.com>
2026-03-04 14:58:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let currentLang = 'ua';
 const availableLangs = ['ua', 'en']; 
-const entityList = ['nasa', 'elon-musk', 'bezos', 'netflix', 'south-sudan', 'us-defence-department', 'tokio'];
+const entityList = ['nasa', 'elon-musk', 'bezos', 'netflix', 'south-sudan', 'us-defence-department', 'tokio', 'european-union'];
 let langDataCache = {}; 
 let currentYear = "2026";
 let currentTimeUnit = "sec";
